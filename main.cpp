e1df03b04cbdfc42187f6cb207f74fcda7a7b2e9
TinongSot <96507767+TinongSot@users.noreply.github.com>
2021-12-25 20:09:25 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@
 #include <fstream>
 #include <limits>
 #include <cstdio>
-#include <ctime>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -21,8 +20,6 @@
 #include <sys/ioctl.h>
 #endif
 
-//testing git pull request
-//kljojoij
 using namespace std;
 
 struct Student {
@@ -62,8 +59,6 @@ void enableEcho();
 unsigned c_rand();
 
 int main() {
-    srand(time(NULL));
-
     unsigned choice;
 
     cout << Tcolors::BOLDGREEN << "Welcome to the student management system!" << Tcolors::RESET << endl;
@@ -91,7 +86,7 @@ int main() {
 
             string line;
             ifstream file("users.txt");
-            
+
             if (!file.is_open()) {
                 cout << Tcolors::RED << "Error: Could not open file." << Tcolors::RESET << endl;
                 cout << endl;
@@ -150,8 +145,73 @@ int main() {
         }
         else if (choice == 2) {
             // login as teacher
-            // Teacher features
-            // createa, read , update, delete
+            string username, password;
+
+            betterCin("Please enter your username: ", username, "Invalid username. Please try again.", false);
+            betterCin("Please enter your password: ", password, "Invalid password. Please try again.", true);
+            cout << endl;
+
+            bool found = false;
+            bool isTeacher = false;
+
+            string line;
+            ifstream file("users.txt");
+
+            if (!file.is_open()) {
+                cout << Tcolors::RED << "Error: Could not open file." << Tcolors::RESET << endl;
+                cout << endl;
+                continue;
+            }
+
+            string user;
+            string hash;
+            string salt;
+            string digest;
+
+            while (getline(file, line)) {
+                user = line.substr(0, line.find(":"));
+                hash = line.substr(line.find(":") + 1);
+
+                if (user == username) {
+                    salt = hash.substr(0, hash.find("."));
+                    digest = hash.substr(hash.find(".") + 1);
+
+                    if (digest == SHA256(password + salt + "isTeacher")) {
+                        found = true;
+                        isTeacher = true;
+                        break;
+                    }
+                    else if (digest == SHA256(password + salt)) {
+                        found = true;
+                        isTeacher = false;
+                        break;
+                    }
+                }
+            }
+
+            file.close();
+
+            if (!found) {
+                cout << Tcolors::RED << "Error: Invalid username or password." << Tcolors::RESET << endl;
+                cout << endl;
+                continue;
+            }
+            else {
+                if (isTeacher) {
+                    cout << Tcolors::YELLOW << "Info: You have entered login information of a teacher." << Tcolors::RESET << endl;
+                    cout << endl;
+                    // Teacher features
+                    // createa, read , update, delete
+                    continue;
+                }
+                else {
+                    cout << Tcolors::YELLOW << "Info: You have entered login information of a student." << Tcolors::RESET << endl;
+                    cout << Tcolors::BLINKGREEN << "Login successful!" << Tcolors::RESET << endl;
+                    cout << endl;
+
+                    continue;
+                }
+            }
         }
         else if (choice == 3) {
             string username, password, confPassword;
@@ -229,6 +289,78 @@ int main() {
         }
         else if (choice == 4) {
             // register as teacher
+            string username, password, confPassword;
+
+            betterCin("Please enter your username: ", username, "Invalid username. Please try again.", false);
+            betterCin("Please enter your password: ", password, "Invalid password. Please try again.", true);
+            cout << endl;
+
+            betterCin("Please enter your password again: ", confPassword, "Invalid password. Please try again.", true);
+            cout << endl;
+
+            if (password != confPassword) {
+                cout << Tcolors::RED << "Error: Passwords do not match." << Tcolors::RESET << endl;
+                cout << endl;
+                continue;
+            }
+
+            bool found = false;
+
+            string line;
+            ifstream file("users.txt");
+
+            if (!file.is_open()) {
+                cout << Tcolors::RED << "Error: Could not open file." << Tcolors::RESET << endl;
+                cout << endl;
+                continue;
+            }
+
+            string user;
+            string hash;
+            string salt;
+            string digest;
+
+            while (getline(file, line)) {
+                user = line.substr(0, line.find(":"));
+                hash = line.substr(line.find(":") + 1);
+
+                if (user == username) {
+                    found = true;
+                    break;
+                }
+            }
+
+            file.close();
+
+            if (found) {
+                cout << Tcolors::RED << "Error: Username already exists." << Tcolors::RESET << endl;
+                continue;
+            }
+            else {
+                vector<char> v_salt(0);
+                for (int i = 0; i < 16; i++) {
+                    v_salt.push_back(c_rand());
+                }
+                string salt = string(v_salt.begin(), v_salt.end());
+
+                digest = SHA256(password + salt + "isTeacher");
+
+                ofstream ofile("users.txt", ios::app);
+
+                if (!ofile.is_open()) {
+                    cout << Tcolors::RED << "Error: Could not open file." << Tcolors::RESET << endl;
+                    cout << endl;
+                    continue;
+                }
+
+                ofile << username << ":" << salt << "." << digest << endl;
+
+                ofile.close();
+
+                cout << Tcolors::BLINKGREEN << "Registration successful!" << Tcolors::RESET << endl;
+                cout << endl;
+                continue;
+            }
         }
         else if (choice == 5) {
             cout << "1. Login as a student" << endl;
@@ -273,41 +405,22 @@ void betterCin(string __Prompt, T& __Return_Buffer, string __Error_Message, bool
 }
 
 // disableEcho() disables writing to the terminal
-#ifdef _WIN32
 void disableEcho() {
     HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
     DWORD mode;
     GetConsoleMode(hStdin, &mode);
     SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
 }
-#else
-void disableEcho() {
-    struct termios tty;
-    tcgetattr(STDIN_FILENO, &tty);
-    tty.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-}
-#endif
 
 // enableEcho() enables writing to the terminal
-#ifdef _WIN32
 void enableEcho() {
     HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
     DWORD mode;
     GetConsoleMode(hStdin, &mode);
     SetConsoleMode(hStdin, mode | ENABLE_ECHO_INPUT);
 }
-#else
-void enableEcho() {
-    struct termios tty;
-    tcgetattr(STDIN_FILENO, &tty);
-    tty.c_lflag |= ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-}
-#endif
 
 // crand() returns a random unsigned integer
-#ifdef _WIN32
 unsigned c_rand() {
     unsigned r, v;
     rand_s(&r);
@@ -319,14 +432,3 @@ unsigned c_rand() {
 
     return v;
 }
-#else
-unsigned c_rand() {
-    unsigned r = (unsigned)rand() % 127;
-
-    if (!(r >= 48 && r <= 57) && !(r >= 65 && r <= 90) && !(r >= 97 && r <= 122)) {
-        r = c_rand();
-    }
-
-    return r;
-}
-#endif
